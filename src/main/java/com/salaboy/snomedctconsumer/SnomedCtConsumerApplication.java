2da2ea8f7e1b44f64f449e163032285161e31e75
salaboy <salaboy@gmail.com>
2019-03-20 15:01:53 -0300

    Initial import

---
+++ src/main/java/com/salaboy/snomedctconsumer/SnomedCtConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.salaboy.snomedctconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnomedCtConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnomedCtConsumerApplication.class, args);
+	}
+
+}
